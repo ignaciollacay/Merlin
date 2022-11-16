8f8b03b5dd10a4eed4974c452930161033135e27
ignaciollacay <ignacio.llacay@gmail.com>
2022-11-16 15:45:12 -0300

    Updated Character attacks
    - Added Character Layer for Particle Collision Detection
    - Replaced Enemy's Attack VFX to AttackSound
    - Added overrides to Enemy's AttackSound (ParticleSystem.Collision & SpellCollision component)
    - Code and inspector values updated in Enemy & Player Prefabs.

--- Assets/Scripts/SpellCollision.cs
+++ Assets/Scripts/SpellCollision.cs
@@ -6,21 +6,30 @@ using UnityEngine;
 //To be added when instantiating the object?
 // or added to the prefabs?
 
+
 public class SpellCollision : MonoBehaviour
 {
     public SpellSO spell;
 
     //public event EventHandler OnSpellHit;
- 
+
 
     private void OnParticleCollision(GameObject other)
     {
-        if (other.CompareTag("Enemy"))
-        {
-            other.GetComponent<EnemyStats>().TakeDamage(spell.damage);
-            other.GetComponent<EnemyController>().RecieveAttack();
+        TakeDamage(other);
+    }
 
-            //OnSpellHit?.Invoke(this, EventArgs.Empty);
+    // TODO: Should send an event
+    // and subscribe from the corresponding stats or controller.
+    private void TakeDamage(GameObject other)
+    {
+        if (other.TryGetComponent(out EnemyStats enemy))
+        {
+            enemy.TakeDamage(spell.damage);
+        }
+        else if (other.TryGetComponent(out PlayerStats player))
+        {
+            player.TakeDamage(spell.damage);
         }
     }
 }
