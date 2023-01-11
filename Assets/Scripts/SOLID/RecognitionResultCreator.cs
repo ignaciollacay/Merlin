ff2829a961637e4df896e7db882b2f7e1133a40f
ignaciollacay <ignacio.llacay@gmail.com>
2023-01-11 10:25:29 -0300

    Update Scroll & Dojo
    Missing references of new Spell Assessment behavior

---
+++ Assets/Scripts/SOLID/RecognitionResultCreator.cs
@@ -0,0 +1,44 @@
+﻿
+public static class RecognitionResultCreator
+{
+    /* Testing class only. Requires to add a RecognitionResult constructor with no parameters.
+     * Commenting out, should use a string and render class obsolete and delete
+     * Keeping in case of need
+     
+    public static RecognitionResult result;
+
+    public static RecognitionResult getCorrectResultAlternatives()
+    {
+        RecognitionResult result = new RecognitionResult();
+        result.Phrases = new RecognizedPhrase[3];
+        RecognizedPhrase phrase1 = new RecognizedPhrase();
+        RecognizedPhrase phrase2 = new RecognizedPhrase();
+        RecognizedPhrase phrase3 = new RecognizedPhrase();
+        result.Phrases[0] = phrase1;
+        result.Phrases[1] = phrase2;
+        result.Phrases[2] = phrase3;
+        result.Phrases[0].Text = "te curan de fortaleza";
+        result.Phrases[1].Text = "te curan de fortalece";
+        result.Phrases[2].Text = "te curen de fortaleza";
+
+        return result;
+    }
+
+    public static RecognitionResult getIncorrectResult()
+    {
+        RecognitionResult result = new RecognitionResult();
+        result.Phrases = new RecognizedPhrase[3];
+        RecognizedPhrase phrase1 = new RecognizedPhrase();
+        RecognizedPhrase phrase2 = new RecognizedPhrase();
+        RecognizedPhrase phrase3 = new RecognizedPhrase();
+        result.Phrases[0] = phrase1;
+        result.Phrases[1] = phrase2;
+        result.Phrases[2] = phrase3;
+        result.Phrases[0].Text = "te curan de fortaleza";
+        result.Phrases[1].Text = "te curan de fortaleza";
+        result.Phrases[2].Text = "te curen de fortaleza";
+
+        return result;
+    }
+    */
+}
--- Assets/Scripts/SOLID/SpeechToTextUI.cs
+++ Assets/Scripts/SOLID/SpeechToTextUI.cs
@@ -249,45 +249,6 @@ public class SpeechToTextUI : MonoBehaviour
         return phrase;
     }
 }
-
-public static class RecognitionResultCreator
-{
-    public static RecognitionResult result;
-
-    public static RecognitionResult getCorrectResultAlternatives()
-    {
-        RecognitionResult result = new RecognitionResult();
-        result.Phrases = new RecognizedPhrase[3];
-        RecognizedPhrase phrase1 = new RecognizedPhrase();
-        RecognizedPhrase phrase2 = new RecognizedPhrase();
-        RecognizedPhrase phrase3 = new RecognizedPhrase();
-        result.Phrases[0] = phrase1;
-        result.Phrases[1] = phrase2;
-        result.Phrases[2] = phrase3;
-        result.Phrases[0].Text = "te curan de fortaleza";
-        result.Phrases[1].Text = "te curan de fortalece";
-        result.Phrases[2].Text = "te curen de fortaleza";
-
-        return result;
-    }
-
-    public static RecognitionResult getIncorrectResult()
-    {
-        RecognitionResult result = new RecognitionResult();
-        result.Phrases = new RecognizedPhrase[3];
-        RecognizedPhrase phrase1 = new RecognizedPhrase();
-        RecognizedPhrase phrase2 = new RecognizedPhrase();
-        RecognizedPhrase phrase3 = new RecognizedPhrase();
-        result.Phrases[0] = phrase1;
-        result.Phrases[1] = phrase2;
-        result.Phrases[2] = phrase3;
-        result.Phrases[0].Text = "te curan de fortaleza";
-        result.Phrases[1].Text = "te curan de fortaleza";
-        result.Phrases[2].Text = "te curen de fortaleza";
-
-        return result;
-    }
-}
 public class RichTextPhrase
 {
     public string phrase;
